5c834dde6d63039573fb67adea16f0499f2abd89
Rubayat26 <rubayat10me@gmail.com>
2022-07-28 12:44:06 -0400

    Excecise Stopwatch Solved

---
+++ Section1/Excercise/Design a Stopwatch/StopWatch/StopWatch/Watch.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace StopWatch
+{
+    public class Watch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private bool _isRunning;
+
+        public void Start()
+        {
+            if (_isRunning)
+            {
+                throw new InvalidOperationException("The watch is already running.");
+            }
+            _startTime = DateTime.Now;
+            _isRunning = true;
+            
+           
+
+        }
+
+        public void Stop()
+        {
+            _stopTime = DateTime.Now;
+            _isRunning = false;
+
+            var timeSpan = _stopTime - _startTime;
+
+            Console.WriteLine(timeSpan.ToString("ss") + " seconds");
+        }
+        
+       
+        
+    }
+       
+}
+    
