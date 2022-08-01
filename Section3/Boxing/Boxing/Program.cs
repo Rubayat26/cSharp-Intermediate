8a48156cef6969b78c179409486bb84246fb8bc4
Rubayat26 <rubayat10me@gmail.com>
2022-08-01 10:36:42 -0400

    Added Comments regarding Boxing and Unboxing

--- Section3/Boxing/Boxing/Program.cs
+++ Section3/Boxing/Boxing/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿ 
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,13 +9,33 @@ namespace Boxing
     {
         static void Main(string[] args)
         {
-            var list = new ArrayList();
-            list.Add(1);
-            list.Add("Mosh");
-            list.Add(DateTime.Today);
+            var list = new ArrayList();  //ArrayList does not have type safety, we can use
+                                         //any kind of values as parameter
+            
+            list.Add(1);                 //here thge Add method takes object value as
+                                         //its parameter, so Boxing happens as 1 is value type
+                                         //Subsequently when we will get the value or cast
+                                         //Unboxing will happen
+            
+            list.Add("Mosh");            // Boxing does not happen for string type because string 
+                                         // itself is refernce type 
+            
+            list.Add(DateTime.Today);    //Again boxing happens for DateTime object as it is also
+                                         // value type 
 
-            var anotherList = new List<int>();
+
+        //    var number = (int)list[1];      //This line will give an invalid exception
+                                              // as we cannot cast a string to an integer
+
+            
+            var anotherList = new List<int>(); //Generic list we constraint the lists property type
+            anotherList.Add(1);             // Here the add method will get integer type value 
+            
             var names = new List<string>();
         }
     }
 }
+
+
+// Both Boxing and Unboxing has Performance Penalty, so better use the
+// generic version if available 
