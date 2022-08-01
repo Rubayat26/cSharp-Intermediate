4049a63f30d37edaa6b7cdf4e58cc1ebce31fbc3
Rubayat26 <rubayat10me@gmail.com>
2022-08-01 10:52:35 -0400

    added excercise pdf and folder

---
+++ Section3/Excercise/Design a Stack/Stack Design/Stack Design/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+namespace StackDesign 
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+}
