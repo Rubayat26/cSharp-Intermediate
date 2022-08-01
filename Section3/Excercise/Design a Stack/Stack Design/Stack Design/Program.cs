eba306ae0a01e56c63463eba0e0542e76a0fad14
Rubayat26 <rubayat10me@gmail.com>
2022-08-01 13:33:56 -0400

    Solved the StackDesign Challenge

--- Section3/Excercise/Design a Stack/Stack Design/Stack Design/Program.cs
+++ Section3/Excercise/Design a Stack/Stack Design/Stack Design/Program.cs
@@ -1,18 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-
-
-
-
-
-namespace StackDesign 
+﻿namespace StackDesign
 {
     class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            var stack1 = new Stack();
+            stack1.Push(1);
+            stack1.Push(2);
+            stack1.Push(3);
+         // stack1.Push(); // throws exception
+
+
+            //Console.WriteLine(stack1.Count) ; //if this Count method is not described here
+                                               //in stack, stack1.Count will not be available
+                                               //in Main method
+
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+        //  Console.WriteLine(stack1.Pop());   //will throw exception, because empty stack
+
+        }
     }
 }
-}
+
+
+
+
+//Here if we have not added the Count method inside the Stack Class, stack1 would
+//not have the Count method in it.  
---
+++ Section3/Excercise/Design a Stack/Stack Design/Stack Design/Stack.cs
@@ -0,0 +1,62 @@
+﻿
+
+using System.Collections;
+
+
+namespace StackDesign
+{
+    public class Stack
+    {
+        ArrayList stack = new ArrayList();
+        public object pop=0;        //if we do not declare k as object 
+
+
+
+        internal void Push()
+        {
+            throw new InvalidOperationException();
+        }
+
+        public void Push(Object obj)
+        {
+            stack.Add(obj);
+            //Console.WriteLine(stack.Count);
+        }
+        
+        public object Pop()
+        {
+            if (stack.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            pop = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+            return pop; 
+               
+        }
+
+        /*
+        public int Count()
+        {
+            return stack.Count;      //if this Count method is not described here
+                                     //in stack, stack1.Count will not be available
+                                     //in Main method
+                                     
+        }
+        */
+
+
+        public void Clear()
+        {
+            stack.Clear();
+        }
+
+       
+    }
+}
+
+
+
+
+//Here if we have not added the Count method inside the Stack Class, stack1 would
+//not have the Count method in it.  
