1c8d1df2422ea5f0545fa0c28e9e98070ff9d97d
Rubayat26 <rubayat10me@gmail.com>
2022-08-02 15:52:48 -0400

    Solving excercise
    - Solved excercise Design a Database Connection
    - Added summary pdf and excercise problem space
    - added comments on solution method

---
+++ Section4/Excercise/Design Database Connection/DesignDatabaseConnection/DesignDatabaseConnection/DbConnection.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+namespace DesignDatabaseConnection
+{
+    public abstract class DbConnection
+    {
+
+        public string ConnectionString { get; set; }     
+        public TimeSpan TimeSpan { get; set; }
+
+        public abstract void Open();   //the open or close method is iplemented inside
+                                       //sqlConnection or oracleConnection
+
+        public abstract void Close();
+
+
+        public DbConnection(string connectionString)
+        {
+            if (String.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("ConnectionString is null");
+            }
+            
+            this.ConnectionString = connectionString;    // setting the parameter value to 
+                                                         // property value 
+            Console.WriteLine("DBConnection established "+ connectionString);
+
+            
+        }
+
+
+    }
+}
---
+++ Section4/Excercise/Design Database Connection/DesignDatabaseConnection/DesignDatabaseConnection/OracleConnection.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+namespace DesignDatabaseConnection
+{
+    public class OracleConnection : DbConnection
+    {
+        public OracleConnection(string connectionString) : base(connectionString)
+        {
+
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("Opening OracleConnection");
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("Closing OracleConnection");
+        }
+    }
+}
+
+
+// In case of inheritance the constructor of the base class is not
+// inheritted normally to the child class. We have to use the base keyword in child classes
+// constructor to get the constructor of the parent class.We also need to pass the needed
+// parameter of the base class constructor, otherwise runtime cannot go from
+// child class two the parent class  
