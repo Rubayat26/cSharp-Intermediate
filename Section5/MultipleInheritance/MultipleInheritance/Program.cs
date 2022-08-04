393bbc27ec4945a022c1e315b31786958fb5f5f9
Rubayat26 <rubayat10me@gmail.com>
2022-08-04 16:06:38 -0400

    Added comments about difference of inheritance and interfaces and Interfaces are not mulitple inheritance

--- Section5/MultipleInheritance/MultipleInheritance/Program.cs
+++ Section5/MultipleInheritance/MultipleInheritance/Program.cs
@@ -8,3 +8,6 @@ namespace MultipleInheritance
         }
     }
 }
+
+
+
--- Section5/MultipleInheritance/MultipleInheritance/UiControl.cs
+++ Section5/MultipleInheritance/MultipleInheritance/UiControl.cs
@@ -2,9 +2,12 @@
 
 namespace MultipleInheritance
 {
-    public class TextBox : UiControl, IDraggable, IDroppable
+    public class TextBox : UiControl, IDraggable, IDroppable   //to write the full block of 
+                                                            // interface press Alt+enter
+                                                            // and then enter again
     {
-        public void Drag()
+        public void Drag()            // here we need all the methods from
+                                      // IDraggable and IDroppable
         {
             throw new NotImplementedException();
         }
@@ -31,3 +34,24 @@ namespace MultipleInheritance
         }
     }
 }
+
+
+// Interface got nothing to do with Inheritance 
+// Here TextBox cannot have multiuple inheritance 
+// but it can connected with multiple interfaces
+
+
+
+// In interafces all we ae doing is confirmin to a contract. We are
+// making sure that the public interface of our class has the methods or
+// members as it is declared in the interface 
+// so if we try to remove drag() from the texbox, it wont compile here 
+
+
+// Interfaces are usedfor building losely coupled extensible and testable 
+// applications; not for code reusability
+// 
+
+
+
+// But intefaces do provide polymorphic behaviour 
