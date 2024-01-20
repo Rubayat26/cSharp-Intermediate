4af0d3941b3edbb679afdbdc1cddd095bde21af7
Rubayat Islam <91696266+Rubayat26@users.noreply.github.com>
2024-01-19 19:52:48 -0500

    Update Vehicle.cs

--- Section3/Constructors/Constructors/Vehicle.cs
+++ Section3/Constructors/Constructors/Vehicle.cs
@@ -6,7 +6,8 @@ namespace Constructors
     {
         private readonly string _registrationNumber;  //private accessModifier is not available in derived classes,
                                                       //So the base class constructor is called inside the constructor of the derived classes
-
+                                                      //If we define it as protected we would be available to access it, but thats against 
+                                                      //the idea of encapsulation 
 //        public Vehicle()
 //        {
 //            Console.WriteLine("Vehicle is being initialized.");
